c19ab29495779043d702911616dc64c30057f390
Devisree Avvaru <devisree.avvarus@gmail.com>
2025-04-16 16:45:24 +0000

    Completed Lab ContainsDuplicates

--- src/main/java/ContainsDuplicates.java
+++ src/main/java/ContainsDuplicates.java
@@ -1,4 +1,4 @@
-
+import java.util.HashSet;
 
 public class ContainsDuplicates {
     /**
@@ -8,6 +8,12 @@ public class ContainsDuplicates {
      * @return true if nums contains any duplicate values, false if it does not.
      */
     public boolean containsDuplicate(int[] nums){
+        HashSet<Integer> set = new HashSet<>();
+        for(int i : nums){
+            if(!set.add(i)){
+                return true;
+            }
+        }
         return false;
     }
 }
